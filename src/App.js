cfaed8356ea3182dcda13240f9b22fb2be453cee
Aleksandar Rusev <a.rusev@hotmail.com>
2019-10-17 00:52:55 +0300

    added animejs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import { CSSTransition } from "react-transition-group";
+import {CSSTransition, Transition} from "react-transition-group";
+import anime from "animejs";
 
 function App() {
-  const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
 
-  useEffect(() => {
+    useEffect(() => {
         setVisible(true);
-  }, []);
+    }, []);
 
-  return (
-    <div className="app">
-      <div className="box-container">
-        <CSSTransition
-          in={visible}
-          mountOnEnter
-          timeout={400}
-          classNames="my-node"
-          unmountOnExit
-        >
-          <div className="box" />
-        </CSSTransition>
-      </div>
-      <div className="btn-container">
-        <button onClick={() => setVisible(!visible)}>Show</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="box-container">
+                <Transition
+                    in={visible}
+                    mountOnEnter
+                    unmountOnExit
+                    timeout={400}
+                    addEndListener={(node, done) => {
+                        const tl = anime.timeline({
+                            complete: done,
+                            duration: 800,
+                            easing: visible ? 'easeInExpo' : 'easeOutExpo',
+                            targets: node,
+                        });
+                        tl.add({
+                            translateX: visible ? [-300, 0] : [0, 300],
+                            opacity: visible ? [0, 1] : [1, 0],
+                            maxHeight: visible ? [0, '100%'] : ['100%', 0],
+                        })
+                    }}
+                >
+                    <div className="box"/>
+                </Transition>
+            </div>
+            <div className="btn-container">
+                <button onClick={() => setVisible(!visible)}>Show</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
