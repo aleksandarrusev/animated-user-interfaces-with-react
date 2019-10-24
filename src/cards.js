c8edf9f55ac3dd67759ce33accf7f464b67c5ed6
Aleksandar Rusev <a.rusev@hotmail.com>
2019-10-25 01:15:06 +0300

    extracted the cards array into separated file

---
+++ src/cards.js
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    title: "Card 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+  },
+  {
+    title: "Card 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+  },
+  {
+    title: "Card 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+  },
+  {
+    title: "Card 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+  },
+  {
+    title: "Card 5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+  },
+  {
+    title: "Card 6",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+  }
+];
