e6d4f3d09857945ce4e6db8affd46ed308a40968
alexanderrusev <alexander.rusev@mentormate.com>
2019-10-24 19:37:34 +0300

    reformatting with Prettier and fixed the errors in the consols

--- src/pages/Anime/Anime.js
+++ src/pages/Anime/Anime.js
@@ -10,13 +10,12 @@ import "../../assets/images/macbook-pro.jpg";
 import isInViewport from "in-viewport";
 
 class Anime extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       show: false,
-      scrolledToCards: false,
-    }
+      scrolledToCards: false
+    };
   }
 
   textBoxRef = React.createRef();
@@ -28,37 +27,36 @@ class Anime extends React.Component {
   arrowRef = React.createRef();
   cardsContainerRef = React.createRef();
 
-
   cards = [
     {
       title: "Card 1",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
     },
     {
       title: "Card 2",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
     },
     {
       title: "Card 3",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
     },
     {
       title: "Card 4",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
     },
     {
       title: "Card 5",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
     },
     {
       title: "Card 6",
       description:
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, nesciunt."
     }
   ];
 
@@ -81,22 +79,24 @@ class Anime extends React.Component {
 
   componentDidMount() {
     this.setState({
-      show: true,
+      show: true
     });
 
-    document.addEventListener('scroll', e => {
-      if (isInViewport(this.cardsContainerRef.current) && !this.state.scrolledToCards) {
+    document.addEventListener("scroll", e => {
+      if (
+        isInViewport(this.cardsContainerRef.current) &&
+        !this.state.scrolledToCards
+      ) {
         this.setState({
-          scrolledToCards: true,
+          scrolledToCards: true
         });
         this.animateCardsReveal();
       }
-
     });
   }
 
   scrollDown = () => {
-    window.scrollBy({top: window.innerHeight + 100, behavior: "smooth"});
+    window.scrollBy({ top: window.innerHeight + 100, behavior: "smooth" });
   };
 
   animateFirstSlide = (el, done) => {
@@ -121,21 +121,21 @@ class Anime extends React.Component {
       duration: 500
     });
     tl.add(
-        {
-          targets: this.scrollBtnRef.current,
-          translateY: [80, 0],
-          duration: 500,
-          complete: () => {
-            anime({
-              targets: ".arrow",
-              translateX: 5,
-              direction: "alternate",
-              loop: true,
-              easing: "easeInOutSine"
-            });
-          }
-        },
-        "-=500"
+      {
+        targets: this.scrollBtnRef.current,
+        translateY: [80, 0],
+        duration: 500,
+        complete: () => {
+          anime({
+            targets: ".arrow",
+            translateX: 5,
+            direction: "alternate",
+            loop: true,
+            easing: "easeInOutSine"
+          });
+        }
+      },
+      "-=500"
     );
     tl.add({
       targets: ".scroll-button",
@@ -144,65 +144,74 @@ class Anime extends React.Component {
   };
 
   render() {
-    const {show} = this.state;
+    const { show } = this.state;
 
     return (
-        <Transition
-            timeout={400}
-            in={show}
-            unmountOnExit
-            addEndListener={this.animateFirstSlide}
-        >
-          <div className="page-container">
-            <div className="scroll-container">
-              <div className="image-box" ref={this.imageBoxRef}>
-                <div className="image-box-overlay" ref={this.imageBoxOverlayRef}/>
-              </div>
-              <div className="text-box" ref={this.textBoxRef}>
-                <h3 className="heading-container">
-              <span className="heading" ref={this.headingRef}>
-                This is how we use Anime.js
-              </span>
-                </h3>
-                <p className="description-container">
-              <span className="description" ref={this.descriptionRef}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aperiam, nesciunt.?
-              </span>
-                </p>
-                <div className="button-container">
-                  <button className="btn-open" ref={this.scrollBtnRef}>
-                    Click here to continue{" "}
-                    <span className="arrow" ref={this.arrowRef}>
-                  &rarr;
+      <Transition
+        timeout={400}
+        in={show}
+        unmountOnExit
+        addEndListener={this.animateFirstSlide}
+      >
+        <div className="page-container">
+          <div className="scroll-container">
+            <div className="image-box" ref={this.imageBoxRef}>
+              <div
+                className="image-box-overlay"
+                ref={this.imageBoxOverlayRef}
+              />
+            </div>
+            <div className="text-box" ref={this.textBoxRef}>
+              <h3 className="heading-container">
+                <span className="heading" ref={this.headingRef}>
+                  This is how we use Anime.js
                 </span>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="scroll-container cards" ref={this.cardsContainerRef}>
-              <TransitionGroup className="cards-container">
-                {this.cards.map(card => (
-                    <div className="card">
-                      <h3 className="card-title">
-                        <span className="card-revealing-text">{card.title} </span>
-                      </h3>
-                      <div className="card-description">
-                  <span className="card-revealing-text">
-                    {card.description}
+              </h3>
+              <p className="description-container">
+                <span className="description" ref={this.descriptionRef}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aperiam, nesciunt.?
+                </span>
+              </p>
+              <div className="button-container">
+                <button className="btn-open" ref={this.scrollBtnRef}>
+                  Click here to continue{" "}
+                  <span className="arrow" ref={this.arrowRef}>
+                    &rarr;
                   </span>
-                      </div>
-                    </div>
-                ))}
-              </TransitionGroup>
-            </div>
-            <div className="scroll-button" onClick={this.scrollDown}>
-              &#8595;
+                </button>
+              </div>
             </div>
           </div>
-        </Transition>
+          <div className="scroll-container cards" ref={this.cardsContainerRef}>
+            <TransitionGroup className="cards-container">
+              <Transition timeout={4000}>
+                <>
+                  {this.cards.map(card => (
+                    <div className="card" key={card.title}>
+                      <h3 className="card-title">
+                        <span className="card-revealing-text">
+                          {card.title}{" "}
+                        </span>
+                      </h3>
+                      <div className="card-description">
+                        <span className="card-revealing-text">
+                          {card.description}
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </>
+              </Transition>
+            </TransitionGroup>
+          </div>
+          <div className="scroll-button" onClick={this.scrollDown}>
+            &#8595;
+          </div>
+        </div>
+      </Transition>
     );
   }
-};
+}
 
 export default Anime;
